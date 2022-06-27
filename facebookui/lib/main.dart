395441551172157d607917c7b6a17cbc516a6e5a
youms <yussef.msabeh@gmail.com>
2022-06-27 12:48:45 +0300

    modified main file

--- facebookui/lib/main.dart
+++ facebookui/lib/main.dart
@@ -3,5 +3,6 @@ import 'package:facebookui/facebookUi2.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: facebookUI()));
+  // runApp(MaterialApp(home: facebookUI()));
+  runApp(MaterialApp(home: facebookUI2()));
 }
