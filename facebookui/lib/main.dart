4bc2c020ee84e34fb8597b04556763de93046fbe
youms <yussef.msabeh@gmail.com>
2022-06-27 10:38:09 +0300

    change the main screen

--- facebookui/lib/main.dart
+++ facebookui/lib/main.dart
@@ -3,5 +3,5 @@ import 'package:facebookui/facebookUi2.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: facebookUI2()));
+  runApp(MaterialApp(home: facebookUI()));
 }
